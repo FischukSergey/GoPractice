01fd0c77ef8326dfe1916e1786b1731717c4f855
Сергей Фищук <sergey.fischuk@mail.ru>
2024-04-06 19:20:33 +0300

    06042024

--- main.go
+++ main.go
@@ -1,68 +1,197 @@
 package main
 
+// импортируйте нужные пакеты
 import (
 	"fmt"
-	// "strconv"
-	// "math"
+	"strconv"
+	"strings"
+	"time"
 )
 
-var Global int = 1234
-var AnotherGlobal = -1111
+const (
+	K1 = 0.035
+	K2 = 0.029
+)
 
-func main() {
-	// var j int
-	// i := Global + AnotherGlobal
-	// fmt.Println("Инициализация пустой переменной j: ", j)
-	// j = Global
-	// // math.Abs() требует параметр float64
-	// // соответственно мы приводим тип
-	// k := math.Abs(float64(AnotherGlobal))
-	// // fmt.Printf("Global = %d, i = %d j = %d k = %.2f,\n", Global, i, j, k)
-	// //conv("молодой падаван", 50)
-	newB := Digitize(100)
-	fmt.Println(newB)
+var (
+	Format     = "20060102 15:04:05" // формат даты и времени
+	StepLength = 0.65                // длина шага в метрах
+	Weight     = 75.0                // вес кг
+	Height     = 1.75                // рост м
+	Speed      = 1.39                // скорость м/с
+)
 
-	// // fmt.Println(ToAlternatingCase("Проверка ТЕКСТА 1234"))
-	// s:="ПРОВЕРКА TEST"
-	// // fmt.Println(MyString(s).IsUpperCase())
-	// fmt.Println(MultiplicationTable(1))
-}
-
-func Digitize(n int) []int {
-	// your code here
-	var b []int
-	if n == 0 {
-		b = append(b, 0)
+// parsePackage разбирает входящий пакет в параметре data.
+// Возвращаемые значения:
+// t — дата и время, указанные в пакете
+// steps — количество шагов
+// ok — true, если время и шаги указаны корректно, и false — в противном случае
+func parsePackage(data string) (t time.Time, steps int, ok bool) {
+	// 1. Разделите строку на две части по запятой в слайс ds
+	ds := strings.Split(data, ",")
+	// 2. Проверьте, чтобы ds состоял из двух элементов
+	if len(ds) != 2 {
+		return
 	}
-	for i := 0; n > 0; i++ {
-		b = append(b, n%10)
-		n = n / 10
+	var err error
+	// получаем время time.Time
+	t, err = time.Parse(Format, ds[0])
+	if err != nil {
+		return
 	}
-	return b
+	// получаем количество шагов
+	steps, err = strconv.Atoi(ds[1])
+	if err != nil || steps < 0 {
+		return
+	}
+	// отмечаем, что данные успешно разобраны
+	ok = true
+	return
 }
 
-/* ТАБЛИЦА УМНОЖЕНИЯ
-func MultiplicationTable(size int) [][]int {
-  result:=make([][]int,size) //инициализируем строку в двумерном срезе
-  for i := range result {    //инициализируем колонку в виде среза для каждого значения в строке
-    result[i] = make([]int, size) //получается двумерный массив
+// stepsDay перебирает все записи слайс, подсчитывает и возвращает
+// общее количество шагов
+func stepsDay(storage []string) int {
+	// тема оптимизации не затрагивается, поэтому можно
+	// использовать parsePackage для каждого элемента списка
+	totalStep := 0
+	for _, str := range storage {
+		_, step, _ := parsePackage(str)
+		// if ok {  //проверка на корректность обработки строки
+		 totalStep += step
+		// }
+	}
+	return totalStep
 }
-	for row:=range result {
-		for column:=range result[row] {
-			result[row][column]=(column+1)*(row+1)
+
+// calories возвращает количество килокалорий, которые потрачены на
+// прохождение указанной дистанции (в метрах) со скоростью 5 км/ч
+func calories(distance float64) float64 {
+	kilocal := (K1*Weight + (Speed*Speed/Height)*K2*Weight) * distance / Speed / 60
+	return kilocal
+}
+
+// achievement возвращает мотивирующее сообщение в зависимости от
+// пройденного расстояния в километрах
+func achievement(distance float64) string {
+	if distance >= 6500 {
+		return "Отличный результат! Цель достигнута."
+	} else if distance >= 3900 {
+		return "Неплохо! День был продуктивный."
+	} else if distance >= 2000 {
+		return "Завтра наверстаем!"
+	} else {
+		return "Лежать тоже полезно. Главное — участие, а не победа!"
+	}
+}
+
+// showMessage выводит строку и добавляет два переноса строк
+func showMessage(s string) {
+	fmt.Printf("%s\n\n", s)
+}
+
+// AcceptPackage обрабатывает входящий пакет, который передаётся в
+// виде строки в параметре data. Параметр storage содержит пакеты за текущий день.
+// Если время пакета относится к новым суткам, storage предварительно
+// очищается.
+// Если пакет валидный, он добавляется в слайс storage, который возвращает
+// функция. Если пакет невалидный, storage возвращается без изменений.
+func AcceptPackage(data string, storage []string) []string {
+	// 1. Используйте parsePackage для разбора пакета
+	t, steps, ok := parsePackage(data)
+	//    выведите сообщение в случае ошибки
+	//    также проверьте количество шагов на равенство нулю
+	if !ok {
+		showMessage("ошибочный формат пакета")
+		return storage
+	}
+	if steps == 0 {
+		return storage
+	}
+	// 2. Получите текущее UTC-время и сравните дни
+	//    выведите сообщение, если день в пакете t.Day() не совпадает
+	//    с текущим днём
+	now := time.Now().UTC()
+	if t.Day() != now.Day() {
+		showMessage(`неверный день`)
+		return storage
+	}
+	// выводим ошибку, если время в пакете больше текущего времени
+	if t.After(now) {
+		showMessage(`некорректное значение времени`)
+		return storage
+	}
+	// проверки для непустого storage
+	if len(storage) > 0 {
+		// 3. Достаточно сравнить первые len(Format) символов пакета с
+		//    len(Format) символами последней записи storage
+		//    если меньше или равно, то ошибка — некорректное значение времени
+		if data[:len(Format)] <= storage[len(storage)-1][:len(Format)] {
+			showMessage(`некорректное значение времени`)
+			return storage
+		}
+
+		// смотрим, наступили ли новые сутки: YYYYMMDD — 8 символов
+		if data[:8] != storage[len(storage)-1][:8] {
+			// если наступили,
+			// то обнуляем слайс с накопленными данными
+			storage = storage[:0]
 		}
 	}
-  return result
+	// остаётся совсем немного
+	// 5. Добавить пакет в storage
+	storage = append(storage, data)
+	// 6. Получить общее количество шагов
+	allSteps := stepsDay(storage)
+	// 7. Вычислить общее расстояние (в метрах)
+	distance := float64(allSteps) * StepLength
+	// 8. Получить потраченные килокалории
+	energy := calories(distance)
+	// 9. Получить мотивирующий текст
+	achiev := achievement(distance)
+	// 10. Сформировать и вывести полный текст сообщения
+	msg := fmt.Sprintf(`Время: %s.
+Количество шагов за сегодня: %d.
+Дистанция составила %.2f км.
+Вы сожгли %.2f ккал.
+%s`, t.Format("15:04:05"), allSteps, distance/1000, energy, achiev)
+	showMessage(msg)
+	// 11. Вернуть storage
+
+	return storage
 }
 
-НУ ИЛИ ТАК
-func MultiplicationTable(size int) [][]int {
-  res := make([][]int, size)
-  for i := 0 ; i < size ; i ++ {
-    for x := 1 ; x < size + 1 ; x ++ {
-      res[i] = append(res[i], (i + 1) * x)
-      }}
-  return res
+func main() {
+	// Вы можете сразу проверить работу функции AcceptPackage
+	// на небольшом тесте.
+	// Если запустить программу после 05:00 UTC, то последнее
+	// сообщение должно быть таким:
+	// Время: 04:45:21.
+	// Количество шагов за сегодня: 16956.
+	// Дистанция составила 11.02 км.
+	// Вы сожгли 664.23 ккал.
+	// Отличный результат! Цель достигнута.
 
+	now := time.Now().UTC()
+	today := now.Format("20060102")
+
+	// данные для самопроверки
+	input := []string{
+		"01:41:03,-100",
+		",3456",
+		"12:40:00, 3456 ",
+		"something is wrong",
+		"02:11:34,678",
+		"02:11:34,792",
+		"17:01:30,1078",
+		"03:25:59,7830",
+		"04:00:46,5325",
+		"04:45:21,3123",
+	}
+
+	var storage []string
+	storage = AcceptPackage("20230720 00:11:33,100", storage)
+	for _, v := range input {
+		storage = AcceptPackage(today+" "+v, storage)
+	}
 }
-*/
